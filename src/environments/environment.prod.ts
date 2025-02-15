a21a88aa6db46a4115814e493586fd13e2afdd78
Jagat <beherajagat713@gmail.com>
2025-02-15 13:51:40 +0530

    url update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://leavesystembackend-production.up.railway.app' // Production URL
+    // baseUrl: 'https://leavesystembackend-production.up.railway.app' // Production URL
+    baseUrl:'http://13.127.130.212:8080'
   };
   
